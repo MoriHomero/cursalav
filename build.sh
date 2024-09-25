c1190c675bfa333d7e97d6330396bf339885c099
hmori <morihomero@gmail.com>
2024-09-25 19:07:21 -0300

    build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,14 @@
 
 set -o errexit
 
+# Instala las dependencias
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin1234') if not User.objects.filter(username='admin').exists() else print('Admin user already exists')"
+# Aplica las migraciones primero
 python manage.py migrate
+
+# Recoge archivos estáticos
+python manage.py collectstatic --noinput
+
+# Crea el superusuario si no existe
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin1234') if not User.objects.filter(username='admin').exists() else print('Admin user already exists')"
